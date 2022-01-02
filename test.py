fb441c225ac0ecc28d6ca5835596255a4e2b4531
irew-j <rlafodwpq@gmail.com>
2022-01-02 20:48:29 +0900

    version_name

---
+++ test.py
@@ -0,0 +1,40 @@
+import requests
+import win32com.client
+
+import win32com.client
+ 
+# 연결 여부 체크
+objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
+bConnect = objCpCybos.IsConnect
+if (bConnect == 0):
+    print("PLUS가 정상적으로 연결되지 않음. ")
+    exit()
+ 
+# 현재가 객체 구하기
+objStockMst = win32com.client.Dispatch("DsCbo1.StockMst")
+objStockMst.SetInputValue(0, 'A005930')   #종목 코드 - 삼성전자
+objStockMst.BlockRequest()
+ 
+# 현재가 통신 및 통신 에러 처리 
+rqStatus = objStockMst.GetDibStatus()
+rqRet = objStockMst.GetDibMsg1()
+print("통신상태", rqStatus, rqRet)
+if rqStatus != 0:
+    exit()
+ 
+# 현재가 정보 조회
+offer = objStockMst.GetHeaderValue(16)  #매도호가
+
+
+
+
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+ 
+myToken = "xoxb-2173600486373-2161919017863-2TIFYx7q3R7GMw5gu4YHi1kr"
+ 
+post_message(myToken,"#stock","삼성전자 현재가:" +  str(offer))
